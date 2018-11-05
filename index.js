80ab9b1c246f7970bfa446a27543e04e7fa2f598
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 16:04:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ function BoardMember(name, homeState, training){
   this.training = training
 
 
-  this
-  this.doCharity = function(){
-    return "I like to help people."
-  }
-
   this.releasePressStatement = function(){
     return "You will see great things from Scuber."
   }
@@ -24,3 +19,6 @@ BoardMember.prototype.veto = function(){
 BoardMember.prototype.approve = function(){
   return 'You can do that!'
 }
+BoardMember.prototype.doCharity = function(){
+    return "I like to help people."
+  }
