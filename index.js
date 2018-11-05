762c06d538b34b003e4482a5a6e399456ee0c104
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 16:05:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-}  
+}
 BoardMember.prototype.veto = function(){
   return 'No, I must disagree'
 }
@@ -15,7 +15,6 @@ BoardMember.prototype.doCharity = function(){
 BoardMember.prototype.releasePressStatement = function(){
   return "You will see great things from Scuber."
 }
-BoardMember.prototypesayHi = function(){
+BoardMember.prototype.sayHi = function(){
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
-
